8a0ab2720eb6ae9dd9b74b60c6a431b52e4841d0
StuartDambi <stuartdambi@gmail.com>
2019-07-22 11:19:22 +0300

    Removed error of number 2 and 3 buttons

--- style.js
+++ style.js
@@ -1,90 +1,86 @@
-// Get the History and Print it on screen
-const getHistory = () => {
-    return document.getElementById('history-value').innerText;
+// Getting and printing the history
+function getHistory(){
+	return document.getElementById("history-value").innerText;
 }
-const printHistory = (num) => {
-    document.getElementById('history-value').innerText = num;
+function printHistory(num){
+	document.getElementById("history-value").innerText=num;
 }
-
-// Get the output and print it on screen
-const getOutput = () => {
-    return document.getElementById('output-value').innerText;
+// Getting and printing of the output from the user
+function getOutput(){
+	return document.getElementById("output-value").innerText;
 }
-const printOutput = (num) => {
-    if (num == '') {
-        document.getElementById('output-value').innerHTML = num;
-    } else {
-        document.getElementById('output-value').innerText = getFormattedNumber(num);
-    }
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}	
 }
-// Format numbers and put commas after three values E.g 1,000,000
-const getFormattedNumber = (num) => {
-    if (num == '-') {
-        return ''; // helps in deleting the negative number
-    }
-    let n = Number(num);
-    let value = n.toLocaleString('en');
-    return value;
+// Format the numbers and put commas after every three digits
+function getFormattedNumber(num){
+	if(num=="-"){
+		return ""; // Deleting negative number solved
+	}
+  
+	let n = Number(num);
+	let value = n.toLocaleString("en");
+	return value;
 }
-// Then remove the commas from the figure displayed for processing
-const reverseNumberFormat = (num) => {
-    return Number(num.replace(/,/g, ''));
+// Then remove the commas for computation
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
 }
-
-// Event listener for Operators
-let operator = document.getElementsByClassName('operator');
-for (let i = 0; i < operator.length; i++) {
-    operator[i].addEventListener('click', function () {
-        // The clear Button
-        if (this.id == 'clear') {
-            printHistory('');
-            printOutput('');
-        }
-        // The delete button
-        if (this.id == 'backspace') {
-            let output = reverseNumberFormat(getOutput()).toString();
-            if (output) {
-                // If output has a value
-                output = output.substr(0, output.length - 1);
-                printOutput(output);
-            }
-        } else {
-            let output = getOutput();
-            let history = getHistory();
-            if(output == '') {output = 0;} //default value
-            if (output == '' && history != '') {
-                if (isNaN(history[history.length - 1])) {
-                    history = history.substr(0, history.length - 1);
-                }
-            }
-            if (output != '' || history != '') {
-                // condition?true:false
-                output = output == '' ?
-                    output : reverseNumberFormat(output);
-                history = history + output;
-                if (this.id == '=') {
-                    let result = eval(history);
-                    printOutput(result);
-                    printHistory('');
-                } else {
-                    history = history + this.id;
-                    printHistory(history);
-                    printOutput('');
-                }
-            }
-        }
-    });
+// Event listeners for the operators
+let operator = document.getElementsByClassName("operator");
+for(let i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
+		}
+		else if(this.id=="backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+				output= output.substr(0,output.length-1);
+				printOutput(output);
+			}
+		}
+		else{
+			var output=getOutput();
+			var history=getHistory();
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substr(0,history.length-1);
+				}
+			}
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
+		}
+		
+	});
 }
 
-// Event listener for Numbers
-let number = document.getElementsByClassName('number');
-for (let i = 0; i < operator.length; i++) {
-    number[i].addEventListener('click', function () {
-        let output = reverseNumberFormat(getOutput());
-        if (output != NaN) {
-            // If output is a number
-            output = output + this.id;
-            printOutput(output);
-        }
-    });
+// Event listeners for the numbers
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
 }
